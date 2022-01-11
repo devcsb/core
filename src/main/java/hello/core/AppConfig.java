a6198a37a56ec9418451755284b96accf6471a6d
devcsb <devcsb119@gmail.com>
2022-01-11 23:14:29 +0900

    Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,19 +14,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig { //AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository()
+
     @Bean
     public MemberService memberService() {  //MemberServiceImpl이란 구현체를 생성시킬 때 MemoryMemberRepository를 생성자 주입 시킨다.
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());  //memoryMemberRepository 객체를 생성하고 그 참조값을 memberServiceImpl을 생성하면서 생성자로 전달한다.
         // -> MemberServiceImpl을 만들고 내가 만든 MemberServiceImpl은 MemoryMemberRepository를 사용할 것이다. 라는 뜻.
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
